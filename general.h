00e11e9298db976994dd6b070b3bdd276874e70f
MunasheEric <munasheeric@gmail.com>
2023-12-12 07:41:50 -0800

    general

--- general.h
+++ general.h
@@ -5,17 +5,17 @@
 #include <stdlib.h>
 #include <string.h>
 
-// Maximum length for a command
+/* Maximum length for a command*/
 #define MAX_COMMAND_LENGTH 1024
 
-// Function to read a line from stdin
+/* Function to read a line from stdin*/
 char* read_line(void);
 
-// Function to parse the line into arguments
+/* Function to parse the line into arguments*/
 char** parse_line(char* line);
 
-// Function to execute the command
+/* Function to execute the command*/
 int execute_command(char** args);
 
-#endif // GENERAL_H
+#endif /* GENERAL_H*/
 
